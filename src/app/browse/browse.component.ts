bdce450bcb91cc230a81eafb0ae0cc42f9b64221
Tyler <claypooltyler00@gmail.com>
2020-06-01 21:08:57 -0500

    browse ts finished

--- src/app/browse/browse.component.ts
+++ src/app/browse/browse.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Shoes } from '../models/shoes';
+import { ShoesService } from '../services/shoes.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-browse',
@@ -7,9 +10,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BrowseComponent implements OnInit {
 
-  constructor() { }
+  shoes: Shoes[] = [];
+  shoe: Shoes;
+  error:string | undefined;
+  errorMessage: string;
+
+  constructor(public service: ShoesService) { }
 
   ngOnInit(): void {
   }
 
+  getShoes() {
+    return this.service.getShoes()
+      .then(
+        shoes => {
+          this.shoes = shoes;
+          this.resetError();
+        },
+        error =>  {
+          this.handleError(error);
+        }
+      );
+  }
+
+  resetError() {
+    this.error = undefined;
+  }
+
+  handleError(error: HttpErrorResponse) {
+    if(error.error instanceof ErrorEvent) {
+      this.error = `An error occurred: ${error.error.message}`; //in the event of a network error. Add error message.
+    } else {
+      this.error = `Backend returned code ${error.status}, body was: ${error.error}`; //If the response status code was an error then display said error
+      }
+    }
 }
