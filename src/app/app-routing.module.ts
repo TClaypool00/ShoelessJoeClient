53673baae1a38b560114d537591876f4598ce3d8
Tyler Claypool <claypooltyler00@gmail.com>
2020-06-04 13:16:40 -0500

    Added shoe details component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component'
 import { BrowseComponent } from './browse/browse.component';
+import { ShoeDetailsComponent } from './browse/shoe-details/shoe-details.component';
 
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'browse', component : BrowseComponent},
+  {path: 'browse/shoe-details', component: ShoeDetailsComponent}
 ];
 
 @NgModule({
