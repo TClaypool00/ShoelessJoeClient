5ee158ff0209a2bb385462171ecc6ffd99317c53
Tyler Claypool <claypooltyler00@gmail.com>
2020-06-06 04:47:23 -0500

    Register almost works

--- src/app/models/users.ts
+++ src/app/models/users.ts
@@ -1,13 +1,13 @@
 export interface Users {
-    UserId: number;
-    FirstName:string;
-    LastName: string;
-    Email: string;
-    Password: string;
-    IsAdmin: boolean;
-    Street: string;
-    City: String;
-    State: string;
-    Zip : number;
-    PhoneNumber: string;
+    userId?: number;
+    firstName:string;
+    lastName: string;
+    email: string;
+    password: string;
+    isAdmin: boolean;
+    street: string;
+    city: String;
+    state: string;
+    zip : number;
+    phoneNumber: string;
 }
