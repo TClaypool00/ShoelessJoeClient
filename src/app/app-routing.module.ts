013745e5d6d4e6d1ea133f10e3251cb4f83ef59c
Tyler Claypool <claypooltyler00@gmail.com>
2020-06-07 00:39:50 -0500

    Added login component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { HomeComponent } from './home/home.component'
 import { BrowseComponent } from './browse/browse.component';
 import { ShoeDetailsComponent } from './browse/shoe-details/shoe-details.component';
 import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'browse', component : BrowseComponent},
   {path: 'shoe-details', component: ShoeDetailsComponent},
   {path: 'register', component: RegisterComponent},
+  {path: 'login', component: LoginComponent },
 ];
 
 @NgModule({
