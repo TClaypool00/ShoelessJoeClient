3a7f20c76dbc166d241496ff09ccafd6c471bdb2
Tyler Claypool <claypooltyler00@gmail.com>
2020-06-07 03:13:50 -0500

    Login might work?

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { first } from 'rxjs/operators';
+import { AlertService, AuthenticationService } from "../services";
 
 @Component({
   selector: 'app-login',
@@ -6,10 +10,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+  loading = false;
+  submitted = false;
+  returnUrl: string;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder, private route: ActivatedRoute, private router: Router, private service: AuthenticationService, private alert: AlertService) {
+    // redirects user if they are alredy logs in
+    if (this.service.currentUserValue) {
+      this.router.navigate(['/'])
+    }
+  }
 
   ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+
+    //get return url from route param or defaults to '/'
+    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+  }
+
+  get f() { return this.loginForm.controls; }
+
+  login() {
+    this.submitted = true;
+
+    //Clears alert message
+    this.alert.clear();
+
+    //Stops if form is invalid
+    if (this.loginForm.invalid) {
+      return;
+    }
+
+    this.loading = true;
+    this.service.login(this.f.email.value, this.f.password.value)
+      .pipe(first())
+      .subscribe(
+        data => {
+          this.router.navigate([this.returnUrl]);
+        },
+        err => {
+          this.alert.error(err);
+          this.loading = false;
+    });
   }
 
 }
