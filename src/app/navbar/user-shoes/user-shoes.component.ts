70aa4730b23216e44a1ef3e1fff7e304dd2105f0
Tyler Claypool <claypooltyler00@gmail.com>
2020-06-08 13:32:09 -0500

    works, css is a little buggy

--- src/app/navbar/user-shoes/user-shoes.component.ts
+++ src/app/navbar/user-shoes/user-shoes.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Shoes, Users } from '../../models';
+import { ShoesService, AuthenticationService } from '../../services';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-shoes',
@@ -6,10 +10,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-shoes.component.css']
 })
 export class UserShoesComponent implements OnInit {
+  shoes: Shoes[] = [];
+  shoe: Shoes;
+  error:string | undefined;
+  errorMessage: string;
+  currentUser:Users;
 
-  constructor() { }
+  constructor(private service: ShoesService, private router: Router, private authService: AuthenticationService) {
+    this.authService.currentUser.subscribe(x => this.currentUser = x);
+   }
 
   ngOnInit(): void {
+    this.getShoes();
   }
+
+  getShoes() {
+    return this.service.getShoes()
+      .then(
+        shoes => {
+          this.shoes = shoes;
+          this.resetError();
+        },
+        error =>  {
+          this.handleError(error);
+        }
+      );
+  }
+
+  getShoe(id:number) {
+    return this.service.getShoeById(id)
+      .then (
+        shoe => {
+          this.shoe = shoe;
+          this.router.navigateByUrl('/shoe-details/' + id);
+        }
+      )
+  }
+
+  resetError() {
+    this.error = undefined;
+  }
+
+  handleError(error: HttpErrorResponse) {
+    if(error.error instanceof ErrorEvent) {
+      this.error = `An error occurred: ${error.error.message}`; //in the event of a network error. Add error message.
+    } else {
+      this.error = `Backend returned code ${error.status}, body was: ${error.error}`; //If the response status code was an error then display said error
+      }
+    }
 
 }
