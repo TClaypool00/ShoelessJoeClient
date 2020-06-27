41635e1e24b0caf441805d186a2cd9c51b943781
Tyler Claypool <claypooltyler00@gmail.com>
2020-06-27 16:13:08 -0500

    user comments css is a little buggy still

--- src/app/navbar/user-comments/user-comments.component.ts
+++ src/app/navbar/user-comments/user-comments.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { CommentService, AuthenticationService } from 'src/app/services';
-import { Users, Comments } from 'src/app/models';
+import { CommentService, AuthenticationService, ShoesService } from 'src/app/services';
+import { Users, Comments, Shoes } from 'src/app/models';
 import { HttpErrorResponse } from '@angular/common/http';
 import { Router } from '@angular/router';
 
@@ -12,23 +12,28 @@ import { Router } from '@angular/router';
 export class UserCommentsComponent implements OnInit {
   currentUser: Users;
   comment: Comments;
-  comments: Comments[] = [];
+  sellComment: Comments;
+  buyComments: Comments[] = [];
+  sellComments: Comments[] = [];
+  currentShoe: Shoes;
+
   error: string | undefined;
   errorMessage: string;
 
-  constructor(private service: CommentService, private authService: AuthenticationService, private router: Router) { 
+  constructor(private service: CommentService, private authService: AuthenticationService, private router: Router, private shoeService: ShoesService) {
     this.authService.currentUser.subscribe(x => this.currentUser = x);
   }
 
   ngOnInit(): void {
     this.getCommentsByCurrentUser();
+    this.getSellCommentsByUser();
   }
 
-  getCommentsByCurrentUser()  {
+  getCommentsByCurrentUser() {
     return this.service.getComments()
       .then(
         comments => {
-          this.comments = comments;
+          this.buyComments = comments;
           this.resetError();
         },
         error => {
@@ -37,11 +42,22 @@ export class UserCommentsComponent implements OnInit {
       )
   }
 
+  getSellCommentsByUser() {
+    return this.service.getComments()
+      .then(sell => {
+        this.sellComments = sell;
+      });
+  }
+
   getComment(id: number) {
     return this.service.getCommentById(id)
-      .then (
+      .then(
         comment => {
           this.comment = comment;
+          this.shoeService.getShoeById(comment.shoeId)
+            .subscribe(shoe => {
+              this.currentShoe = shoe;
+            })
           this.router.navigateByUrl('/comments-details/' + id);
         }
       )
@@ -52,10 +68,10 @@ export class UserCommentsComponent implements OnInit {
   }
 
   handleError(error: HttpErrorResponse) {
-    if(error.error instanceof ErrorEvent) {
+    if (error.error instanceof ErrorEvent) {
       this.error = `An error occurred: ${error.error.message}`; //in the event of a network error. Add error message.
     } else {
       this.error = `Backend returned code ${error.status}, body was: ${error.error}`; //If the response status code was an error then display said error
-      }
     }
+  }
 }
